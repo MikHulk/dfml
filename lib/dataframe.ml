ce224f83d36b6a0d9fb8b450f5052189b7b9b54d
MV <m.viey@wanadoo.fr>
2024-03-04 23:18:48 +0100

    feat: add types

--- lib/dataframe.ml
+++ lib/dataframe.ml
@@ -1,6 +1,56 @@
+type column =
+  | IntegerC of int array
+  | NumericC of int * int array
+  | StringC of string array
+
+type ftype =
+  | Integer of int
+  | Numeric of (int * int)
+  | String of string
+
 type 'a dataset =
   | Empty
-  | Data of int * int * 'a array array
+  | Data of int * int * column array
+
+
+let to_seq (col: column) =
+  match col with
+    IntegerC arr -> Seq.map (fun v -> Integer v) (Array.to_seq arr)
+  | NumericC (p, arr) -> Seq.map (fun v -> Numeric (p, v)) (Array.to_seq arr)
+  | StringC arr -> Seq.map (fun v -> String v) (Array.to_seq arr)
+
+let unwrap_int (field: ftype): int option =
+  match field with
+  | Integer x -> Some(x)
+  | Numeric (p, x) -> Some(x / (p * 10))
+  | String _ -> None
+let%test _ = unwrap_int (Integer 42) = Some 42
+let%test _ = unwrap_int (Numeric (1, 42)) = Some 4
+let%test _ = unwrap_int (String "42") = None
+
+let integer_of_list ll =
+  IntegerC (Array.of_list ll)
+
+let%test _ =
+  match integer_of_list [1;2;3;4] with
+    IntegerC _ -> true
+  | _ -> false
+
+let numeric_of_list precision ll =
+  NumericC (precision, Array.of_list ll)
+
+let%test _ =
+  match numeric_of_list 2 [156;232;367;432] with
+    NumericC (p, arr) -> p = 2 && arr.(1) = 232
+  | _ -> false
+
+let string_of_list ll =
+  StringC (Array.of_list ll)
+
+let%test _ =
+  match string_of_list ["un";"deux";"trois";"quatre"] with
+    StringC arr -> arr = [|"un";"deux";"trois";"quatre"|]
+  | _ -> false
 
 
 let enumerate s = Seq.mapi (fun i x -> (i, x)) s
@@ -13,23 +63,28 @@ let of_list (ll: 'a list list): 'a dataset =
     let height = List.length h in
     let width = List.length ll in
     if List.for_all (fun l -> height = List.length l) t
-    then Data (height, width, (Array.of_list (List.map Array.of_list ll)))
+    then Data (height, width, (Array.of_list (List.map integer_of_list ll)))
     else raise (Invalid_argument "column must have the same size")
 
 let%test _ = match of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]] with
     Data _ -> true
   | _ -> false
 
+let get_cell col row =
+  match col with
+    IntegerC arr -> Integer arr.(row)
+  | NumericC (p, arr) -> Numeric (p, arr.(row))
+  | StringC arr -> String arr.(row)
 
 let get ds col row =
   match ds with
     Empty -> None
   | Data (h, w, arr) ->
     if col < w && row < h
-    then Some arr.(col).(row)
+    then Some (get_cell arr.(col) row)
     else None
 
-let%test _ = get (of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]]) 2 4 = Some(10)
+let%test _ = get (of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]]) 2 4 = Some(Integer 10)
 let%test _ = get (of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]]) 2 5 = None
 
 
@@ -37,11 +92,12 @@ let get' ds col row =
   match ds with
     Empty -> raise (Invalid_argument "empty dataset")
   | Data (_, _, arr) ->
-    arr.(col).(row)
+    get_cell arr.(col) row
 
-let%test _ = get' (of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]]) 2 4 = 10
+let%test _ = get' (of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]]) 2 4 = Integer 10
+
 let%test _ = try
-    get' (of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]]) 2 5 = 10
+    get' (of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]]) 2 5 = Integer (Random.int 100)
   with Invalid_argument _ -> true
      | _ -> false
 
@@ -54,8 +110,13 @@ let get_column ds col =
     then Some arr.(col)
     else None
 
-let%test _ = get_column (of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]]) 2 = Some([|2;8;3;9;10|])
-let%test _ = get_column (of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]]) 3 = None
+let%test _ =
+  get_column (of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]]) 2
+  = Some(IntegerC [|2;8;3;9;10|])
+
+let%test _ =
+  get_column (of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]]) 3
+  = None
 
 
 let get_row ds row =
@@ -63,12 +124,36 @@ let get_row ds row =
     Empty -> None
   | Data (h, _, arr) ->
     if row < h
-    then Some (Array.map (fun arr' -> Array.get arr' row) arr)
+    then Some (Array.map (fun col -> get_cell col row) arr)
     else None
 
-let%test _ = get_row (of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]]) 2 = Some([|3;2;3|])
-let%test _ = get_row (of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]]) 2 <> Some([|1;1;2|])
-let%test _ = get_row (of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]]) 0 = Some([|1;1;2|])
+let%test _ =
+  get_row (
+    of_list
+      [ [1;2;3;4;5]
+      ; [1;1;2;2;2]
+      ; [2;8;3;9;10]
+      ]
+  ) 2 = Some([|Integer 3;Integer 2;Integer 3|])
+    
+let%test _ =
+  get_row (
+    of_list
+      [ [1;2;3;4;5]
+      ; [1;1;2;2;2]
+      ; [2;8;3;9;10]
+      ]
+  ) 2 <> Some([|Integer 1;Integer 1;Integer 2|])
+    
+let%test _ =
+  get_row (
+    of_list
+      [ [1;2;3;4;5]
+      ; [1;1;2;2;2]
+      ; [2;8;3;9;10]
+      ]
+  ) 0 = Some([|Integer 1;Integer 1;Integer 2|])
+    
 let%test _ = get_row (of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]]) 5 = None
 
 
@@ -77,25 +162,26 @@ let filter_col f s =
     (enumerate s)
 
 
-let select (f: int * 'a array -> bool) (ds: 'a dataset): (int * 'a array) Seq.t =
+let select (f: int * column -> bool) (ds: 'a dataset): (int * column) Seq.t =
   match ds with
     Empty -> Seq.empty
   | Data (_, _, arr) -> filter_col f (Array.to_seq arr)
 
-
-let get_all (seq: (int * 'a array) Seq.t): (int * 'a array) list =
+let get_all (seq: (int * column) Seq.t): (int * column) list =
   List.rev (Seq.fold_left (fun acc (i, a) -> (i, a) :: acc) [] seq)
 
 let%test _ = get_all (
     select
-      (fun (_, a) -> Seq.fold_left ( + ) 0 (Array.to_seq a) > 8)
+      (fun (_, a) -> Seq.fold_left ( + ) 0 (Seq.filter_map unwrap_int (to_seq a)) > 8)
       (of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]])
-  ) = [(0, [|1;2;3;4;5|]);(2, [|2;8;3;9;10|])]
+  ) = [(0, IntegerC [|1;2;3;4;5|]);(2, IntegerC [|2;8;3;9;10|])]
+
 let%test _ = get_all (
     select
       (fun _ -> true)
       (of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]])
-  ) = [(0, [|1;2;3;4;5|]);(1, [|1;1;2;2;2|]);(2, [|2;8;3;9;10|])]
+  ) = [(0, IntegerC [|1;2;3;4;5|]);(1, IntegerC [|1;1;2;2;2|]);(2, IntegerC [|2;8;3;9;10|])]
+
 let%test _ = get_all (
     select
       (fun _ -> false)
@@ -103,7 +189,7 @@ let%test _ = get_all (
   ) = []
 
 
-let filter (col_id: int) (f: 'a * 'a -> bool) (ds: 'a dataset): 'a Seq.t =
+let filter (col_id: int) (f: int * ftype -> bool) (ds: 'a dataset): 'a Seq.t =
   match ds with
     Empty -> Seq.empty
   | _ ->
@@ -111,7 +197,7 @@ let filter (col_id: int) (f: 'a * 'a -> bool) (ds: 'a dataset): 'a Seq.t =
       match get_column ds col_id with
         Some col ->
         begin
-          Seq.map (fun (i, _) -> i) (filter_col f (Array.to_seq col))
+          Seq.map (fun (i, _) -> i) (filter_col f (to_seq col))
         end
       | None -> Seq.empty
     end
@@ -133,32 +219,45 @@ let%test _ = List.rev (
         (of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]])
     )
   ) = []
+      
 let%test _ = List.rev (
     Seq.fold_left ( fun acc x -> x::acc) [] (
       filter
         0
-        (fun (_, v) -> v > 2)
+        (fun (_, field) ->
+           match unwrap_int field with
+             Some(v) -> v >  2
+           | _ -> false
+        )
         (of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]])
     )
   ) = [2;3;4]
 
 
-let ( *: ) (s: 'a dataset -> (int * 'a array) Seq.t) (f: 'a dataset -> 'a Seq.t) =
+let ( *: ) (s: 'a dataset -> (int * column) Seq.t) (f: 'a dataset -> 'a Seq.t) =
   fun (ds: 'a dataset) ->
   begin
     let rows = f ds
     in Seq.map (
-      fun row -> List.rev (Seq.fold_left (fun acc (_, a) -> a.(row)::acc) [] (s ds))
+      fun row -> List.rev (Seq.fold_left (fun acc (_, col) -> (get_cell col row)::acc) [] (s ds))
     ) rows
   end
 
 let%test _ =
   let ds = of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]] in
   let sele = select (fun (i, _) -> i = 0 || i = 1) in
-  let filt = filter 2 (fun (_, v) -> v > 5) in
+  let filt =
+    filter 2
+      ( fun (_, field) ->
+          match (unwrap_int field) with
+            Some v -> v > 5
+          | None -> false
+      )
+  in
   List.of_seq (
     (sele *: filt) ds
-  ) = [[2;1];[4;2];[5;2]]
+  ) = [[Integer 2;Integer 1];[Integer 4;Integer 2];[Integer 5;Integer 2]]
+      
 let%test _ =
   let ds = of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]] in
   let sele = select (fun (i, _) -> i = 0 || i = 1) in
@@ -166,31 +265,45 @@ let%test _ =
   List.of_seq (
     (sele *: filt) ds
   ) = []
+
 let%test _ =
   let ds = of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]] in
   let sele = select (fun _ -> false) in
-  let filt = filter 2 (fun (_, v) -> v > 5) in
+  let filt = filter 2
+      ( fun (_, f) ->
+          match (unwrap_int f) with
+            Some v -> v > 5
+          | None -> false
+      )
+  in
   List.of_seq (
     (sele *: filt) ds
   ) = [[];[];[]]
 
 
-let ( +: ) (s: 'a dataset -> (int * 'a array) Seq.t) (f: 'a dataset -> 'a Seq.t) =
+let ( +: ) (s: 'a dataset -> (int * column) Seq.t) (f: 'a dataset -> 'a Seq.t) =
   fun (ds: 'a dataset) ->
   begin
     let rows = f ds
     in Seq.map (
-      fun (_, col) -> List.rev (Seq.fold_left (fun acc row -> col.(row)::acc) [] rows) 
+      fun (_, col) -> List.rev (Seq.fold_left (fun acc row -> (get_cell col row)::acc) [] rows) 
     ) (s ds)
   end
 
 let%test _ =
   let ds = of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]] in
   let sele = select (fun (i, _) -> i = 0 || i = 1) in
-  let filt = filter 2 (fun (_, v) -> v > 5) in
+  let filt = filter 2
+      ( fun (_, f) ->
+          match unwrap_int f with
+            Some v -> v > 5
+          | None -> false
+      )
+  in
   List.of_seq (
     (sele +: filt) ds
-  ) = [[2;4;5];[1;2;2]]
+  ) = [[Integer 2;Integer 4;Integer 5];[Integer 1;Integer 2;Integer 2]]
+      
 let%test "fail" = (
     let ds = of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]] in
     let sele = select (fun (i, _) -> i = 0 || i = 1) in
@@ -199,17 +312,28 @@ let%test "fail" = (
       (sele +: filt) ds
     ) = [[];[]]
   )
+  
 let%test _ =
   let ds = of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]] in
   let sele = select (fun _ -> false) in
-  let filt = filter 2 (fun (_, v) -> v > 5) in
+  let filt = filter 2
+      ( fun (_, f) ->
+          match unwrap_int f with
+            Some v -> v > 5
+          | None -> false
+      )
+  in
   List.of_seq (
     (sele +: filt) ds
   ) = []
 
+let map f col rowid =
+  match col with
+    IntegerC arr -> arr.(rowid) <- (f arr.(rowid))
+  | _ -> ()
 
 let transform
-    (s: 'a dataset -> (int * 'a array) Seq.t)
+    (s: 'a dataset -> (int * column) Seq.t)
     (f: 'a dataset -> 'a Seq.t)
     (t: 'a -> 'a)
     (ds: 'a dataset)
@@ -220,15 +344,25 @@ let transform
     fun rowid ->
       Seq.iter (
         fun (_, col) ->
-          col.(rowid) <- (t col.(rowid))
+          map t col rowid
       ) cols
   ) rows
 
 let%test _ =
   let ds = of_list [[1;2;3;4;5];[1;1;2;2;2];[2;8;3;9;10]] in
   let sele = select (fun (i,_) -> i = 1) in
-  let filt = filter 2 (fun (_, v) -> v > 5) in
+  let filt = filter 2
+      ( fun (_, f) ->
+          match unwrap_int f with
+            Some v -> v > 5
+          | None -> false
+      )
+  in
   transform sele filt (fun x -> x + 2) ds;
   List.of_seq (
     ((select (fun _ -> true)) +: (filter 0 (fun _ -> true))) ds
-  ) = [[1;2;3;4;5];[1;3;2;4;4];[2;8;3;9;10]]
+  ) =
+  [ [Integer 1;Integer 2;Integer 3;Integer 4;Integer 5]
+  ; [Integer 1;Integer 3;Integer 2;Integer 4;Integer 4]
+  ; [Integer 2;Integer 8;Integer 3;Integer 9;Integer 10]
+  ]
--- lib/dataframe.mli
+++ lib/dataframe.mli
@@ -1,23 +1,34 @@
+type ftype =
+  | Integer of int
+  | Numeric of (int * int)
+  | String of string
+
+type column =
+  | IntegerC of int array
+  | NumericC of int * int array
+  | StringC of string array
+
 type 'a dataset =
   | Empty
-  | Data of int * int * 'a array array
-val of_list: 'a list list -> 'a dataset
-val get: 'a dataset -> int -> int -> 'a option
-val get_column: 'a dataset -> int -> 'a array option
-val get_row: 'a dataset -> int -> 'a array option
-val select: (int * 'a array -> bool) -> 'a dataset -> (int * 'a array) Seq.t
-val get_all: (int * 'a array) Seq.t -> (int * 'a array) list
-val filter: int -> (int * int -> bool) -> int dataset -> int Seq.t
+  | Data of int * int * column array
+
+val of_list: int list list -> int dataset
+val get: 'a dataset -> int -> int -> ftype option
+val get_column: 'a dataset -> int -> column option
+val get_row: 'a dataset -> int -> ftype array option
+val select: (int * column -> bool) -> 'a dataset -> (int * column) Seq.t
+val get_all: (int * column) Seq.t -> (int * column) list
+val filter: int -> (int * ftype -> bool) -> int dataset -> int Seq.t
 val ( *: ):
-  (int dataset -> (int * int array) Seq.t)
+  (int dataset -> (int * column) Seq.t)
   -> (int dataset -> int Seq.t)
-  -> (int dataset -> int list Seq.t)
+  -> (int dataset -> ftype list Seq.t)
 val ( +: ):
-  (int dataset -> (int * int array) Seq.t)
+  (int dataset -> (int * column) Seq.t)
   -> (int dataset -> int Seq.t)
-  -> (int dataset -> int list Seq.t)
+  -> (int dataset -> ftype list Seq.t)
 val transform:
-  (int dataset -> (int * int array) Seq.t)
+  (int dataset -> (int * column) Seq.t)
   -> (int dataset -> int Seq.t)
   -> (int -> int)
   -> int dataset
