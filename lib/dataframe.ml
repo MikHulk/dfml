ba9ca77caaf37505dd1375dd9aaa0a7b87aa8fba
MV <m.viey@wanadoo.fr>
2024-03-16 14:17:04 +0100

    feat: Serie module

--- lib/dataframe.ml
+++ lib/dataframe.ml
@@ -17,13 +17,67 @@ module Ftype = struct
   let%test _ = num_of_float 2 42.352 = Numeric(2, 4235)
 
   let of_string s = String s
+  let%test _ = of_string "hello" = String "hello"
 
 end
 
-type serie =
-  | Source of Ftype.t array
-  | Derived of Ftype.t Seq.t
+module Serie = struct
+
+  type t =
+    | Source of Ftype.t array
+    | Derived of Ftype.t Seq.t
+
+  let of_int_seq seq =
+    let arr = Seq.map Ftype.of_int seq |> Array.of_seq in
+    Source arr
+  let%test _ =
+    List.to_seq [ 1; 2; 3; 4; 5 ]
+    |> of_int_seq
+       = Source [| Integer 1; Integer 2; Integer 3; Integer 4; Integer 5 |]
+
+  let nums_of_int_seq p seq =
+    let arr = Seq.map (Ftype.num_of_int p) seq |> Array.of_seq in
+    Source arr
+  let%test _ =
+    List.to_seq [ 1; 2; 3; 4; 5 ]
+    |> nums_of_int_seq 1
+       = Source
+         [| Numeric(1, 1)
+          ; Numeric(1, 2)
+          ; Numeric(1, 3)
+          ; Numeric(1, 4)
+          ; Numeric(1, 5)
+         |]
+
+  let nums_of_float_seq p seq =
+    let arr = Seq.map (Ftype.num_of_float p) seq |> Array.of_seq in
+    Source arr
+  let%test _ =
+    List.to_seq [ 0.1; 0.2; 0.3; 0.4; 0.5 ]
+    |> nums_of_float_seq 1
+       = Source
+         [| Numeric(1, 1)
+          ; Numeric(1, 2)
+          ; Numeric(1, 3)
+          ; Numeric(1, 4)
+          ; Numeric(1, 5)
+         |]
+
+  let of_string_seq seq =
+    let arr = Seq.map Ftype.of_string seq |> Array.of_seq in
+    Source arr
+  let%test _ =
+    List.to_seq [ "1"; "2"; "3"; "4"; "5" ]
+    |> of_string_seq
+       = Source
+         [| String "1"
+          ; String "2"
+          ; String "3"
+          ; String "4"
+          ; String "5"
+         |]
+end
 
 module IntSet = Set.Make(Int)
 
-type dataframe = serie list * IntSet.t
+type dataframe = Serie.t list * IntSet.t
--- lib/dataframe.mli
+++ lib/dataframe.mli
@@ -1,8 +1,8 @@
-module IntSet : sig
+module IntSet: sig
   type t
 end
 
-module Ftype : sig
+module Ftype: sig
   type t =
     | Integer of int
     | Numeric of (int * int)
@@ -14,8 +14,15 @@ module Ftype : sig
   val of_string: string -> t
 end
 
-type serie =
-  | Source of Ftype.t array
-  | Derived of Ftype.t Seq.t
-      
-type dataframe = serie list * IntSet.t
+module Serie: sig
+  type t =
+    | Source of Ftype.t array
+    | Derived of Ftype.t Seq.t
+
+  val of_int_seq: int Seq.t -> t
+  val nums_of_int_seq: int -> int Seq.t -> t
+  val nums_of_float_seq: int -> float Seq.t -> t
+  val of_string_seq: string Seq.t -> t
+end
+
+type dataframe = Serie.t list * IntSet.t
