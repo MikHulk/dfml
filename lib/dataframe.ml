e0b58cfd33b048e0c3a5b9103a0f99c736b181f6
MV <m.viey@wanadoo.fr>
2024-03-16 13:45:49 +0100

    feat: Ftype module

--- lib/dataframe.ml
+++ lib/dataframe.ml
@@ -1,12 +1,29 @@
+module Ftype = struct
+
+  type t =
+    | Integer of int
+    | Numeric of (int * int)
+    | String of string
+
+  let of_int x = Integer x
+  let%test _ = of_int 42 = Integer 42
+
+  let num_of_int p x = Numeric(p, x)
+  let%test _ = num_of_int 2 4200 = Numeric(2, 4200)
+
+  let num_of_float p x =
+    Numeric(p, Float.to_int(Float.round(x *. 10. ** (Float.of_int p))))
+  let%test _ = num_of_float 2 42.356 = Numeric(2, 4236)
+  let%test _ = num_of_float 2 42.352 = Numeric(2, 4235)
+
+  let of_string s = String s
+
+end
+
+type serie =
+  | Source of Ftype.t array
+  | Derived of Ftype.t Seq.t
+
 module IntSet = Set.Make(Int)
 
-type ftype =
-  | Integer of int
-  | Numeric of (int * int)
-  | String of string
-           
-type serie =
-  | Source of ftype array
-  | Derived of ftype Seq.t
-      
 type dataframe = serie list * IntSet.t
--- lib/dataframe.mli
+++ lib/dataframe.mli
@@ -2,13 +2,20 @@ module IntSet : sig
   type t
 end
 
-type ftype =
-  | Integer of int
-  | Numeric of (int * int)
-  | String of string
-           
+module Ftype : sig
+  type t =
+    | Integer of int
+    | Numeric of (int * int)
+    | String of string
+
+  val of_int: int -> t
+  val num_of_int: int -> int -> t
+  val num_of_float: int -> float -> t
+  val of_string: string -> t
+end
+
 type serie =
-  | Source of ftype array
-  | Derived of ftype Seq.t
+  | Source of Ftype.t array
+  | Derived of Ftype.t Seq.t
       
 type dataframe = serie list * IntSet.t
