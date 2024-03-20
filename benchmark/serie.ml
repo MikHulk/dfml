10a4ef7edf4a95c5d53f1ec9bdcc4dfd805acdb7
MV <m.viey@wanadoo.fr>
2024-03-20 09:49:25 +0100

    test: improves stress test

--- benchmark/serie.ml
+++ benchmark/serie.ml
@@ -1,4 +1,4 @@
-let it = 80_000_000
+let it = 800_000_000
 
 let time f =
     let t = Sys.time() in
@@ -6,27 +6,31 @@ let time f =
     Printf.printf "Execution time: %fs\n" (Sys.time() -. t)
     
 let integer_serie_stress_test () =
-  let data = Array.init it (fun _ -> Random.int 100) in
-  let f = fun () -> Array.to_seq data
-      |> Dataframe.Serie.of_int_seq
-      |> Dataframe.Serie.derive
-        ( Dataframe.Ftype.from_int_to_int ( ( * ) 50) ) in
+  let f = fun () ->
+    Seq.repeat 100
+    |> Seq.take it
+    |> Dataframe.Serie.of_int_seq
+    |> Dataframe.Serie.derive
+      ( Dataframe.Ftype.from_int_to_int ( ( * ) 50) ) in
   time f
 
 let numeric_serie_stress_test () =
-  let data = Array.init it (fun _ -> Random.int 100) in
-  let f = fun () -> Array.to_seq data
-      |> Dataframe.Serie.nums_of_int_seq 2
-      |> Dataframe.Serie.derive
-        ( Dataframe.Ftype.from_float_to_float cos ) in
+  let f = fun () ->
+    Seq.repeat 100
+    |> Seq.take it
+    |> Dataframe.Serie.nums_of_int_seq 2
+    |> Dataframe.Serie.derive
+      ( Dataframe.Ftype.from_float_to_float cos ) in
   time f
 
 let string_serie_stress_test () =
-  let data = Array.init it (fun _ -> Random.int 100) in
-  let f = fun () -> Array.to_seq data
-      |> Dataframe.Serie.nums_of_int_seq 2
-      |> Dataframe.Serie.derive
-        (Dataframe.Ftype.from_int_to_str Int.to_string) in
+  let f = fun () ->
+    Seq.repeat 100
+    |> Seq.take it
+    |> Seq.map Random.int
+    |> Dataframe.Serie.nums_of_int_seq 2
+    |> Dataframe.Serie.derive
+      (Dataframe.Ftype.from_int_to_str Int.to_string) in
   time f
 
 let () =
@@ -36,35 +40,34 @@ let () =
   
   print_string "===========================================================";
   print_newline () ;
-  print_string "integer serie stress test";
-  print_newline () ;
+  Printf.printf "%#d integers serie stress test\n" it;
   integer_serie_stress_test ();
   let (minor', major', promo') = Gc.counters () in
   let consumption = (minor' -. minor) +. (major' -. major) -. (promo' -. promo) in
   let (minor, major, promo) = (minor', major', promo') in
-  Printf.printf "memory consumption: %fwords\n" consumption;
+  Printf.printf "memory consumption: %#fwords\n" consumption;
   print_newline () ;
   print_string "===========================================================";
   print_newline () ;
   
-  print_string "numeric serie stress test";
+  Printf.printf "%#d numerics serie stress test\n" it;
   print_newline () ;
   numeric_serie_stress_test ();
   let (minor', major', promo') = Gc.counters () in
   let consumption =
     (minor' -. minor) +. (major' -. major) -. (promo' -. promo) in
   let (minor, major, promo) = (minor', major', promo') in
-  Printf.printf "memory consumption: %fwords\n" consumption;
+  Printf.printf "memory consumption: %#fwords\n" consumption;
   print_newline () ;
   print_string "===========================================================";
   print_newline () ;
   
-  print_string "string serie stress test";
+  Printf.printf "%#d strings serie stress test\n" it;
   print_newline () ;
   string_serie_stress_test ();
   let (minor', major', promo') = Gc.counters () in
   let consumption = (minor' -. minor) +. (major' -. major) -. (promo' -. promo) in
-  Printf.printf "memory consumption: %fwords\n" consumption;
+  Printf.printf "memory consumption: %#fwords\n" consumption;
   print_newline () ;
   print_string "===========================================================";
   print_newline () ;
